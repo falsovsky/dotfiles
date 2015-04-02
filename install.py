0bf7e6c8e30311c5745826281dd15e95aa9f09c8
Pedro de Oliveira <falso@localhost.localdomain>
2015-04-02 23:52:45 +0100

    install script

--- install.py
+++ install.py
@@ -4,6 +4,17 @@
 
 import os
 import json
+import sys
+
+home = os.path.expanduser("~")
+dotfiles = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 with open('symlinks.json') as data_file:    
     symlinks = json.load(data_file)
+
+for symlink in symlinks:
+    src = os.path.join(dotfiles, symlink)
+    dst = os.path.join(home, symlinks[symlink])
+    print(src + " -> " + dst)
+    os.symlink(src, dst)
+
